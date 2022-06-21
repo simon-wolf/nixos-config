a7ffcc8095a5b2862864c4e90cd8046041a69ec3
Simon Wolf <swolf@fastmail.co.uk>
2022-06-21 16:46:29 +0100

    Configure Zsh prompt

--- modules/shell/zsh.nix
+++ modules/shell/zsh.nix
@@ -11,7 +11,18 @@
       dotDir = ".config/zsh_nix";
       enableAutosuggestions = true;
       enableSyntaxHighlighting = true;
-      history.size = 10000;
+      history.size = 10000;                 # Maximum events in internal history
+      history.save = 10000;                 # Maximum events in history file
+
+      initExtra = ''
+        # Prompt
+        autoload -U promp_purification_setup; prompt_purification_setup
+      '';
     };
   };
+
+  home.file.".config/zsh_nix/external/prompt_purification_setup" = {
+    source = ../../config/zsh/external/prompt_purification_setup;
+  };
+
 }
