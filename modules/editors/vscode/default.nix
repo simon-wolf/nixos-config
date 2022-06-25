2bc4823bcd1fa3b12ffb1bb3e412bec1ef65d43f
Simon Wolf <swolf@fastmail.co.uk>
2022-06-25 10:25:44 +0100

    Tidy up the VSCode config

--- modules/editors/vscode/default.nix
+++ modules/editors/vscode/default.nix
@@ -16,19 +16,19 @@ let
       name = "vscode-elixir-credo";
       publisher = "pantajoe";
       version = "0.7.2";
-      sha256 = "036lknj1vs42g1zxsbzh692czl6rkzpffvqn15n5yz4ps33gvhy5"    ;
+      sha256 = "036lknj1vs42g1zxsbzh692czl6rkzpffvqn15n5yz4ps33gvhy5";
     }
     {
       name = "phoenix";
       publisher = "phoenixframework";
       version = "0.1.1";
-      sha256 = "0pd1s6f1lm96y1q9d9aj7sa8n0vc0cjrggiyyh1azwa5h59v1w01"    ;
+      sha256 = "0pd1s6f1lm96y1q9d9aj7sa8n0vc0cjrggiyyh1azwa5h59v1w01";
     }
     {
       name = "uuid-generator";
       publisher = "netcorext";
       version = "0.0.5";
-      sha256 = "01mhzmc8ck2rk8nr9pqgkp2qk508ssx8asis27ll9vjmw38liiys"    ;
+      sha256 = "01mhzmc8ck2rk8nr9pqgkp2qk508ssx8asis27ll9vjmw38liiys";
     }
   ];
 
