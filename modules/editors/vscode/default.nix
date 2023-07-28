e2d545cfd95a245d70941caebdae38e0d3c9de56
Simon Wolf <swolf@fastmail.co.uk>
2023-07-28 18:16:34 +0100

    Update VSCode packages

--- modules/editors/vscode/default.nix
+++ modules/editors/vscode/default.nix
@@ -6,35 +6,26 @@
 
 let
   vscode_extensions = (with pkgs.vscode-extensions; [
+    bradlc.vscode-tailwindcss
     jakebecker.elixir-ls
     ms-python.python
     ms-python.vscode-pylance
+    phoenixframework.phoenix
     stkb.rewrap
     tomoki1207.pdf
   ]) ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
     {
       name = "vscode-elixir-credo";
       publisher = "pantajoe";
-      version = "0.7.2";
-      sha256 = "036lknj1vs42g1zxsbzh692czl6rkzpffvqn15n5yz4ps33gvhy5";
-    }
-    {
-      name = "phoenix";
-      publisher = "phoenixframework";
-      version = "0.1.1";
-      sha256 = "0pd1s6f1lm96y1q9d9aj7sa8n0vc0cjrggiyyh1azwa5h59v1w01";
+      version = "0.8.3";
+#      sha256 = "0000000000000000000000000000000000000000000000000000";
+      sha256 = "sha256-l4IlCd/S4NSwpY+/fhHOshj621Op8n5N1G0GvCVzzsE=";
     }
     {
       name = "uuid-generator";
       publisher = "netcorext";
       version = "0.0.5";
       sha256 = "01mhzmc8ck2rk8nr9pqgkp2qk508ssx8asis27ll9vjmw38liiys";
-    }
-    {
-      name = "vscode-tailwindcss";
-      publisher = "bradlc";
-      version = "0.8.6";
-      sha256 = "v15KuD3eYFCsrworCJ1SZAMkyZKztAwWKmfwmbirleI=";
     }
   ];
 
