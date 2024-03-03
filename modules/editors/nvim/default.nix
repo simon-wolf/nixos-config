2583022953fd063cc1b46b5c658346381e7e7eb8
Simon Wolf <swolf@fastmail.co.uk>
2024-03-03 13:44:07 +0000

    Use Treesitter for syntax highlighting

--- modules/editors/nvim/default.nix
+++ modules/editors/nvim/default.nix
@@ -1,6 +1,12 @@
 #
 # Neovim
 #
+# See:
+# 
+# https://gist.github.com/nat-418/d76586da7a5d113ab90578ed56069509
+#
+# https://gist.github.com/nat-418/493d40b807132d2643a7058188bff1ca
+#
 
 { pkgs, ... }:
 
@@ -10,17 +16,33 @@
       enable = true;
       viAlias = true;
       vimAlias = true;
+      vimdiffAlias = true;
 
       plugins = with pkgs.vimPlugins; [
         fzfWrapper
         lazygit-nvim
         nerdtree
         nerdtree-git-plugin
-        vim-elixir
-        vim-git
-        vim-json
-        vim-markdown
-        vim-nix
+        (nvim-treesitter.withPlugins (
+	  plugins: with plugins; [
+	    bash
+	    csv
+	    dockerfile
+	    elixir
+	    erlang
+	    heex
+	    html
+	    javascript
+	    jq
+	    json
+	    markdown
+	    markdown_inline
+	    nix
+	    python
+	    ssh_config
+	    xml
+	  ]
+	))
       ];
       
       extraConfig = ''
@@ -29,7 +51,7 @@
         set number					" Show line numbers
         set nofoldenable
         set spelllang=en_gb
-        set spell
+        set nospell
 
         let NERDTreeMinimalUI=1
 
