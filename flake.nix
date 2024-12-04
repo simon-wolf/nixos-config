3f41bf7654416ab345db4003ece5151d36361161
Simon Wolf <swolf@fastmail.co.uk>
2024-12-04 19:54:55 +0000

    Update to NixOS 24.11

--- flake.nix
+++ flake.nix
@@ -2,10 +2,10 @@
   description = "Simon Wolf's NixOS and Home-Manager Configuration";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
 
     home-manager = {
-      url = "github:nix-community/home-manager/release-24.05";
+      url = "github:nix-community/home-manager/release-24.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
