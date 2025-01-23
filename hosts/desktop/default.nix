ed83f377be1def0052bfc04ea704770fe46f495f
Simon Wolf <swolf@fastmail.co.uk>
2025-01-23 10:22:02 +0000

    Tidy some comments

--- hosts/desktop/default.nix
+++ hosts/desktop/default.nix
@@ -23,11 +23,11 @@
 
   virtualisation.libvirtd.enable = true;
 
-  # Enable TPM emulation (optional)
-#  virtualisation.libvirtd.qemu = {
+  virtualisation.libvirtd.qemu = {
+    # Enable TPM emulation (optional)
 #    swtpm.enable = true;
 #    ovmf.packages = [ pkgs.OVMFFull.fd ];
-#  };
+  };
 
   # Enable USB redirection (optional)
   virtualisation.spiceUSBRedirection.enable = true;
