82524e70237e96642573fc8781de5d08b0f0a816
Simon Wolf <swolf@fastmail.co.uk>
2025-04-27 22:30:46 +0100

    Neovim config changes

--- modules/editors/nvim/default.nix
+++ modules/editors/nvim/default.nix
@@ -20,6 +20,7 @@
 
       plugins = with pkgs.vimPlugins; [
         fzfWrapper
+        fzf-vim
         lazygit-nvim
         nerdtree
         nerdtree-git-plugin
@@ -46,6 +47,10 @@
 	    xml
 	  ]
 	))
+	# lualine
+	nvim-web-devicons
+	lualine-nvim
+	
 	vim-snipmate
         vim-snippets
       ];
@@ -56,6 +61,8 @@
         set nofoldenable
         set spelllang=en_gb
         set nospell
+
+        nnoremap <C-p> :Files<Cr>
 
         let NERDTreeMinimalUI=1
 
@@ -81,6 +88,88 @@
 
       extraLuaConfig = ''
         vim.opt.number = true
+
+        require('lualine').setup {
+          options = {
+            icons_enabled = true,
+            theme = 'everforest',
+            component_separators = { left = '', right = ''},
+            section_separators = { left = '', right = ''},
+            disabled_filetypes = {
+              statusline = {},
+              winbar = {}
+            },
+            ignore_focus = {},
+            always_divide_middle = true,
+            always_show_tabline = true,
+            globalstatus = false,
+            refresh = {
+              statusline = 100,
+              tabline = 100,
+              winbar = 100
+            }
+          },
+          sections = {
+            lualine_a = {'mode'},
+            lualine_b = {'branch', 'diff', 'diagnostics'},
+            lualine_c = {
+              {
+                'buffers',
+                show_filename_only = true,   -- Shows shortened relative path when set to false.
+                hide_filename_extension = false,   -- Hide filename extension when set to true.
+                show_modified_status = true, -- Shows indicator when the buffer is modified.
+
+                mode = 4, -- 0: Shows buffer name
+                          -- 1: Shows buffer index
+                          -- 2: Shows buffer name + buffer index
+                          -- 3: Shows buffer number
+                          -- 4: Shows buffer name + buffer number
+
+                max_length = vim.o.columns * 2 / 3, -- Maximum width of buffers component,
+                                                    -- it can also be a function that returns
+                                                    -- the value of `max_length` dynamically.
+                filetype_names = {
+                  TelescopePrompt = 'Telescope',
+                  dashboard = 'Dashboard',
+                  packer = 'Packer',
+                  fzf = 'FZF',
+                  alpha = 'Alpha',
+                }, -- Shows specific buffer name for that filetype ( { `filetype` = `buffer_name`, ... } )
+
+                -- Automatically updates active buffer color to match color of other components (will be overidden if buffers_color is set)
+                use_mode_colors = true,
+
+                -- buffers_color = {
+                  -- Same values as the general color option can be used here.
+                --   active = 'lualine_{section}_normal',     -- Color for active buffer.
+                --   inactive = 'lualine_{section}_inactive', -- Color for inactive buffer.
+                -- },
+
+                symbols = {
+                  modified = ' ●',      -- Text to show when the buffer is modified
+                  alternate_file = '#', -- Text to show to identify the alternate file
+                  directory =  '',     -- Text to show when the buffer is a directory
+                },
+              },
+            },
+
+            lualine_x = {'encoding'},
+	    lualine_y = {'progress'},
+            lualine_z = {'location'}
+          },
+          inactive_sections = {
+            lualine_a = {},
+            lualine_b = {},
+            lualine_c = {'filename'},
+            lualine_x = {'location'},
+            lualine_y = {},
+            lualine_z = {}
+          },
+          tabline = {},
+          winbar = {},
+          inactive_winbar = {},
+          extensions = {}
+        }
       '';
     };
   };
