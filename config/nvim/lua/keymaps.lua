f9aee25ee31c06f75e91d61037040a1ddd453fcb
Simon Wolf <swolf@fastmail.co.uk>
2025-05-11 12:32:28 +0100

    Add keymapping to launch FzfLua showing all built-in search types

--- config/nvim/lua/keymaps.lua
+++ config/nvim/lua/keymaps.lua
@@ -2,4 +2,6 @@ vim.keymap.set('n', '<C-p>', '<cmd>FzfLua files<Cr>', { silent = true, desc = "F
 
 vim.keymap.set('n', '<leader><C-p>', '<cmd>FzfLua live_grep<Cr>', { silent = true, desc = "Find by grepping current working directory" })
 
+vim.keymap.set('n', '<leader><C-o>', '<cmd>FzfLua builtin<Cr>', { silent = true, desc = "Find from a list of all FzfLua search types" })
+
 vim.keymap.set('n', '-', '<cmd>Oil --float<Cr>', { silent = true, desc = "Open parent directory in Oil" })
