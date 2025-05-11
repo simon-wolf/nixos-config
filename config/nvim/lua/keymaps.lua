80dc934616ccc295d377ce5d9fc6b28905ccdb20
Simon Wolf <swolf@fastmail.co.uk>
2025-05-11 13:05:00 +0100

    Add more FzfLua keybindings

--- config/nvim/lua/keymaps.lua
+++ config/nvim/lua/keymaps.lua
@@ -1,7 +1,15 @@
+-- FzfLua
 vim.keymap.set('n', '<C-p>', '<cmd>FzfLua files<Cr>', { silent = true, desc = "Find files in the current working directory" })
+vim.keymap.set('n', '<leader>ff', '<cmd>FzfLua files<Cr>', { silent = true, desc = "[F]ind [F]iles" })
+vim.keymap.set('n', '<leader>fg', '<cmd>FzfLua live_grep<Cr>', { silent = true, desc = "[F]ind [G]rep" })
+vim.keymap.set('n', '<leader>fa', '<cmd>FzfLua builtin<Cr>', { silent = true, desc = "[F]ind via [a]ll FzfLua search types" })
+vim.keymap.set('n', '<leader>fh', '<cmd>FzfLua helptags<Cr>', { silent = true, desc = "[F]ind in Neovim [H]elp" })
+vim.keymap.set('n', '<leader>fk', '<cmd>FzfLua keymaps<Cr>', { silent = true, desc = "[F]ind [K]eymaps" })
+vim.keymap.set('n', '<leader>fw', '<cmd>FzfLua grep_cword<Cr>', { silent = true, desc = "[F]ind current [W]ord" })
+vim.keymap.set('n', '<leader>fW', '<cmd>FzfLua grep_cWORD<Cr>', { silent = true, desc = "[F]ind current [W]ORD" })
+vim.keymap.set('n', '<leader>fr', '<cmd>FzfLua resume<Cr>', { silent = true, desc = "[F]ind [R]esume" })
+vim.keymap.set('n', '<leader>fo', '<cmd>FzfLua oldfiles<Cr>', { silent = true, desc = "[F]ind [O]ld Files (last opened)" })
+vim.keymap.set('n', '<leader><leader>', '<cmd>FzfLua buffers<Cr>', { silent = true, desc = "Find buffers" })
 
-vim.keymap.set('n', '<leader><C-p>', '<cmd>FzfLua live_grep<Cr>', { silent = true, desc = "Find by grepping current working directory" })
-
-vim.keymap.set('n', '<leader><C-o>', '<cmd>FzfLua builtin<Cr>', { silent = true, desc = "Find from a list of all FzfLua search types" })
-
+-- Oil
 vim.keymap.set('n', '-', '<cmd>Oil --float<Cr>', { silent = true, desc = "Open parent directory in Oil" })
--- config/nvim/plugin/project.lua
+++ config/nvim/plugin/project.lua
@@ -1,4 +1,6 @@
 -- https://github.com/ahmedkhalf/project.nvim/
+--
+-- This plugin allows FzfLua to start a files search from the project's root folder.
 
 require("project_nvim").setup()
 
