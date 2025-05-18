1c5401200ad6af3de3be3f20caa3c3b6fe506d9c
Simon Wolf <swolf@fastmail.co.uk>
2025-05-18 16:13:30 +0100

    Add an lsp nvim config file

---
+++ config/nvim/plugin/lsp.lua
@@ -0,0 +1,9 @@
+vim.lsp.enable('lua-language-server')
+-- lua-language-server
+
+vim.lsp.config('lsp', {
+  -- Server-specific settings. See `:help lsp-quickstart`
+  settings = {
+--    ['rust-analyzer'] = {},
+  },
+})
