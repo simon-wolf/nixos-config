ec4ddf915874f17dfd34b97c0e52458993b1156a
Simon Wolf <swolf@fastmail.co.uk>
2025-07-17 18:09:24 +0100

    Comment out something which should be in home-manager config

--- hosts/desktop/default.nix
+++ hosts/desktop/default.nix
@@ -26,12 +26,13 @@
     virt-manager.enable = true;
   };
 
-  dconf.settings = {
-    "org/virt-manager/virt-manager/connections" = {
-      autoconnect = ["qemu:///system"];
-      uris = ["qemu:///system"];
-    };
-  };
+  # See https://nixos.wiki/wiki/Virt-manager
+  # dconf.settings = {
+  #   "org/virt-manager/virt-manager/connections" = {
+  #     autoconnect = ["qemu:///system"];
+  #     uris = ["qemu:///system"];
+  #   };
+  # };
 
   fileSystems."/mnt/pi-nas/shared" = {
     device = "//192.168.10.2/shared";
