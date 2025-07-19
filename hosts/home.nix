49598fb851d31a7e4db3cd09d0b9d32871c709c5
Simon Wolf <swolf@fastmail.co.uk>
2025-07-19 21:37:53 +0100

    Tweak Emacs config

--- hosts/home.nix
+++ hosts/home.nix
@@ -132,6 +132,7 @@
         (setq standard-indent 2)
         (setq treesit-extra-load-path ("/run.current-system/sw/lib/"))
         (load-theme 'dracula t)
+	(global-set-key [remap list-buffers] 'ibuffer)
       '';
     };
   };
