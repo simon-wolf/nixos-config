3cde23749bf7b2a4752b0de2c7f4c9a75d256521
name <swolf@fastmail.co.uk>
2025-12-18 21:19:09 +0000

    Fix Git config

--- modules/shell/git.nix
+++ modules/shell/git.nix
@@ -7,7 +7,7 @@
     git = {
       enable = true;
       settings = {
-        user.name = "name";
+        user.name = "Simon Wolf";
         user.email = "swolf@fastmail.co.uk";
         pull.rebase = true;
         init.defaultBranch = "main";
