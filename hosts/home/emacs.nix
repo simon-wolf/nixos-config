1fa61c850297259caaea1f258912fe21b70afff7
Simon Wolf <swolf@fastmail.co.uk>
2026-04-21 21:12:31 +0100

    Add multiple-cursors package

--- hosts/home/emacs.nix
+++ hosts/home/emacs.nix
@@ -14,6 +14,7 @@
         epkgs.lsp-pyright
         # epkgs.eglot
         epkgs.magit
+        epkgs.multiple-cursors
         epkgs.nix-mode
         epkgs.tree-sitter
         epkgs.tree-sitter-langs
